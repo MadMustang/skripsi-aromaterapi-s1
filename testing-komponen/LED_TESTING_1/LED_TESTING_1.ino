887e5e7e12b7cfaf67304b9aab1a8bc4c43e0a3c
Richard <richardpapz@gmail.com>
2019-03-04 15:26:36 +0700

    LED
    LED Testing use it for Relay but fail

--- testing-komponen/LED_TESTING_1/LED_TESTING_1.ino
+++ testing-komponen/LED_TESTING_1/LED_TESTING_1.ino
@@ -1,22 +1,21 @@
 #include<FastLED.h>
 #include "SoftwareSerial.h"
-#define NUM_LEDS 100
+#define NUM_LEDS 1
 #define LED_PIN0 5
 
 CRGB leds[NUM_LEDS];
 
-int ledPin = 3;
+
 
 void setup()
 {
   // put your setup code here, to run once:
-
   FastLED.addLeds<WS2812B, LED_PIN0, GRB>(leds, NUM_LEDS);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:  
   for (int i = 0; i < 255; i++)
   {
     setColor(i);
